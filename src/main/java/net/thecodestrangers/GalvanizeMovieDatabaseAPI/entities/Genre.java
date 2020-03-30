7945507cab37767a1331a9d35187eee6b5050c77
David3Ar <davide.arnesano@gmx.com>
2020-03-30 12:56:08 +0200

    entity Movie created

---
+++ src/main/java/net/thecodestrangers/GalvanizeMovieDatabaseAPI/entities/Genre.java
@@ -0,0 +1,5 @@
+package net.thecodestrangers.GalvanizeMovieDatabaseAPI.entities;
+
+public enum Genre {
+
+}
---
+++ src/main/java/net/thecodestrangers/GalvanizeMovieDatabaseAPI/entities/Movie.java
@@ -0,0 +1,71 @@
+package net.thecodestrangers.GalvanizeMovieDatabaseAPI.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+class Movie {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private int releaseYear; 
+    private Genre genre;
+    private int runtimeInMinutes;
+    private int rating;
+
+
+    public Movie() {}
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReleaseYear() {
+        return this.releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Genre getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public int getRuntimeInMinutes() {
+        return this.runtimeInMinutes;
+    }
+
+    public void setRuntimeInMinutes(int runtimeInMinutes) {
+        this.runtimeInMinutes = runtimeInMinutes;
+    }
+
+    public int getRating() {
+        return this.rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }    
+}
