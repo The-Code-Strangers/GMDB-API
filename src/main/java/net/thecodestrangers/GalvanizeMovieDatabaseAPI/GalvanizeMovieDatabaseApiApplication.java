4cdcff5b6ce98f718f33f0f3da3642582919ce12
Christian Sonntag <Christian@Christians-Air.fritz.box>
2020-03-30 10:01:59 +0200

    First Springboot Initial App

---
+++ src/main/java/net/thecodestrangers/GalvanizeMovieDatabaseAPI/GalvanizeMovieDatabaseApiApplication.java
@@ -0,0 +1,13 @@
+package net.thecodestrangers.GalvanizeMovieDatabaseAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalvanizeMovieDatabaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalvanizeMovieDatabaseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/thecodestrangers/GalvanizeMovieDatabaseAPI/GalvanizeMovieDatabaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thecodestrangers.GalvanizeMovieDatabaseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalvanizeMovieDatabaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
